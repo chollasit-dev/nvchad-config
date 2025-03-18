fc2b21542005cf5904e50c65c9bdcd5388a2f2ba
chollasit-dev <118034127+chollasit-dev@users.noreply.github.com>
2025-03-18 16:00:55 +0700

    feat(keymap): add custom keymaps for built-in shortcuts and plugins
    - Add custom built-in keymap for closing the current tab
    - Add a lot of shortcut keymaps for neogit
    - Add a few keymaps for todo comments, and noice

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -4,7 +4,45 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
-map("i", "jk", "<ESC>")
-
+-- examples
+-- map("i", "jk", "<ESC>")
 -- map("n", ";", ":", { desc = "CMD enter command mode" })
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+-- my keymaps
+map("n", "<leader>X", ":tabclose<CR>", { desc = "Close current tab" })
+
+-- neogit
+map("n", "<leader>gc", ":Neogit commit<CR>", { desc = "Neogit commit" })
+map("n", "<leader>gP", ":Neogit push<CR>", { desc = "Neogit push" })
+map("n", "<leader>gS", ":Neogit stash<CR>", { desc = "Neogit stash" })
+map("n", "<leader>gb", ":Telescope git_branches<CR>", { desc = "View branches" })
+map("n", "<leader>gd", ":Neogit diff<CR>", { desc = "Neogit diff" })
+map("n", "<leader>gf", ":Neogit fetch<CR>", { desc = "Neogit fetch" })
+map("n", "<leader>gl", ":Neogit log<CR>", { desc = "Neogit log" })
+map("n", "<leader>gp", ":Neogit pull<CR>", { desc = "Neogit pull" })
+map("n", "<leader>gg", ":Neogit<CR>", { desc = "Open neogit" })
+
+-- gitsign
+map("n", "<leader>gB", ":Gitsigns blame<CR>", { desc = "Open git blame" })
+
+-- todo comments
+local todo = require "todo-comments"
+map("n", "<leader>tt", ":TodoTelescope<CR>", { desc = "Todo Telescope" })
+
+map("n", "]t", function()
+  todo.jump_next()
+end, { desc = "Next todo comment" })
+
+map("n", "[t", function()
+  todo.jump_prev()
+end, { desc = "Previous todo comment" })
+
+-- noice
+map("n", "<leader>dd", ":Noice dismiss<CR>", { desc = "Noice dismiss" })
+
+-- can also specify a list of valid jump keywords
+
+-- vim.keymap.set("n", "]t", function()
+--   require("todo-comments").jump_next { keywords = { "ERROR", "WARNING" } }
+-- end, { desc = "Next error/warning todo comment" })
