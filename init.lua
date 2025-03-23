3d7da518f5059a5310b53c95e43f24d40b420336
chollasit-dev <118034127+chollasit-dev@users.noreply.github.com>
2025-03-23 17:34:02 +0700

    feat(nvchad): set to show dashboard when no buffer left

--- init.lua
+++ init.lua
@@ -32,6 +32,16 @@ dofile(vim.g.base46_cache .. "statusline")
 require "options"
 require "nvchad.autocmds"
 
+-- show nvchad dashboard when no buffer
+vim.api.nvim_create_autocmd("BufDelete", {
+  callback = function()
+    local bufs = vim.t.bufs
+    if #bufs == 1 and vim.api.nvim_buf_get_name(bufs[1]) == "" then
+      vim.cmd "Nvdash"
+    end
+  end,
+})
+
 vim.schedule(function()
   require "mappings"
 end)
