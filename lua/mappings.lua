40e76a1c04431bdc4a4234d9a778b33d7b3c7223
chollasit-dev <118034127+chollasit-dev@users.noreply.github.com>
2025-04-13 17:38:21 +0700

    feat(keymaps): modify and add more keymaps
    - Change how I categorize my keymaps from based on installed plugins to
      action types, e.g., tab, find, search, etc.
    - Add more mappings similarly to LazyVim keybindings for ease of migration
      between 2 Neovim distros

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,48 +1,58 @@
 require "nvchad.mappings"
 
--- add yours here
-
+-- set
 local map = vim.keymap.set
 
--- my keymaps
-map("n", "<leader>X", ":tabclose<CR>", { desc = "Close current tab" })
+-- quick command
+map("n", "<leader>`", ":bNext<CR>", { desc = "Go to next buffer" })
+map("n", "<leader>n", ":Telescope notify<CR>", { desc = "Telescope notify history" })
 
--- telescope
-map("n", "<leader>fd", ":Telescope diagnostics<CR>", { desc = "telescope find diagnostics" })
-map("n", "<leader>fc", ":Telescope git_commits<CR>", { desc = "telescope find commit" })
+-- quit
+map("n", "<leader>qq", ":qa!<CR>", { desc = "Quit Neovim all force" })
 
--- neogit
-map("n", "<leader>gc", ":Neogit commit<CR>", { desc = "Neogit commit" })
+-- tab
+map("n", "<leader>dd", ":tabclose<CR>", { desc = "Close current tab" })
+
+-- find
+map("n", "<leader>:", ":Telescope command_history<CR>", { desc = "Telescope command history" })
+map("n", "<leader>fc", ":Telescope git_commits<CR>", { desc = "Telescope find commit" })
+
+-- search
+map("n", "<leader>sC", ":Telescope commands<CR>", { desc = "Telescope search commands" })
+map("n", "<leader>sd", ":Telescope diagnostics<CR>", { desc = "Telescope search diagnostics" })
+map("n", "<leader>st", ":TodoTelescope<CR>", { desc = "Todo Telescope" })
+map("n", "<leader>sw", ":Telescope live_grep<CR>", { desc = "Telescope live grep" })
+
+-- ui
+map("n", "<leader>un", ":Noice dismiss<CR>", { desc = "Noice dismiss" })
+map("n", "<leader>fe", ":NvimTreeToggle<CR>", { desc = "Neovim tree toggle" })
+
+-- git
+map("n", "<leader>gB", ":Gitsigns blame<CR>", { desc = "Gitsigns blame" })
 map("n", "<leader>gP", ":Neogit push<CR>", { desc = "Neogit push" })
 map("n", "<leader>gS", ":Neogit stash<CR>", { desc = "Neogit stash" })
 map("n", "<leader>gb", ":Telescope git_branches<CR>", { desc = "View branches" })
+map("n", "<leader>gc", ":Neogit commit<CR>", { desc = "Neogit commit" })
 map("n", "<leader>gd", ":Neogit diff<CR>", { desc = "Neogit diff" })
 map("n", "<leader>gf", ":Neogit fetch<CR>", { desc = "Neogit fetch" })
+map("n", "<leader>gg", ":Neogit<CR>", { desc = "Open neogit tab" })
 map("n", "<leader>gl", ":Neogit log<CR>", { desc = "Neogit log" })
 map("n", "<leader>gp", ":Neogit pull<CR>", { desc = "Neogit pull" })
-map("n", "<leader>gg", ":Neogit<CR>", { desc = "Open neogit" })
-
--- gitsign
-map("n", "<leader>gB", ":Gitsigns blame<CR>", { desc = "Open git blame" })
+map("n", "<leader>gs", ":Telescope git_status<CR>", { desc = "Telescope git status" })
 
 -- fterm
-vim.keymap.set("n", "<A-i>", '<CMD>lua require("FTerm").toggle()<CR>', { noremap = true })
-vim.keymap.set("t", "<A-i>", '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>', { noremap = true })
+vim.keymap.set({ "n", "t" }, "<A-i>", function()
+  require("FTerm").toggle()
+end)
 
 -- todo comments
 local todo = require "todo-comments"
-map("n", "<leader>tt", ":TodoTelescope<CR>", { desc = "Todo Telescope" })
-
 map("n", "]t", function()
   todo.jump_next()
 end, { desc = "Next todo comment" })
-
 map("n", "[t", function()
   todo.jump_prev()
 end, { desc = "Previous todo comment" })
-
--- noice
-map("n", "<leader>dd", ":Noice dismiss<CR>", { desc = "Noice dismiss" })
 
 -- can also specify a list of valid jump keywords
 
