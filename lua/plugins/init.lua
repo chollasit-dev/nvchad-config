48412b4253549e22e37bd746ebc8328bdd24db75
chollasit-dev <118034127+chollasit-dev@users.noreply.github.com>
2025-04-16 00:13:07 +0700

    feat(db): add and configure plugin to interact with database
    - Add DBUI plugin for ease of use
    - Set notification output via nvim-notify plugin since default is
      unreadable and meant to be use with light theme
    - Add keymaps for toggling DBUI

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -108,6 +108,33 @@ return {
     event = { "bufEnter *.py" },
   },
 
+  -- database UI
+  {
+    "kristijanhusak/vim-dadbod-ui",
+    cmd = {
+      "DBUI",
+      "DBUIToggle",
+      "DBUIAddConnection",
+      "DBUIFindBuffer",
+    },
+    dependencies = {
+      { "tpope/vim-dadbod", lazy = true },
+      { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+    },
+    init = function()
+      vim.g.db_ui_use_nerd_fonts = 1
+      vim.g.db_ui_use_nvim_notify = 1
+      vim.g.db_ui_winwidth = 30
+    end,
+    keys = {
+      {
+        "<leader>D",
+        "<cmd>DBUI<CR>",
+        desc = "toggle DBUI",
+      },
+    },
+  },
+
   -- markup qol
   {
     "windwp/nvim-ts-autotag",
